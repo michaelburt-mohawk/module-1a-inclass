f9211a5be2ddaf91025608fe127f902f9fe3d31b
michaelburt-mohawk <michael.burt@mohawkcollege.ca>
2024-09-10 19:16:56 -0400

    Added C# command line project

---
+++ TestApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// Test application
+
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Write your name:");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"You typed: {name}");
+
+int someNumber;
+Console.WriteLine("Write in a number:");
+
+
+someNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Some number is " + someNumber);
+Console.WriteLine($"Some number + 5 = {someNumber + 5}");
